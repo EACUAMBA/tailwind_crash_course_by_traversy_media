2c03ad3278083f5dd3e772057d44cd4f8fcbbf81
Edilson A. Cuamba <edilsoncuamba@gmail.com>
2022-06-21 12:33:04 +0200

    Adicionando scripts no package.json para executar o tailwind e fazer o build.

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //Indicamos onde o tailwind deverá procurar ficheiros html com as classes para ele compilar. Estamos dizendo que será no mesmo nivel onde temos este ficheiro de configuração.
+  content: ['./*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
